e820965a51632d2e0bef9a1a0e8f4c4358a736c0
Jonathan <julli626@gmail.com>
2024-08-08 17:18:38 +0300

    for pussy

--- CampusIL/campusil_7.2.6.py
+++ CampusIL/campusil_7.2.6.py
@@ -35,7 +35,7 @@ def add_item_fromlist(string): #$ 6 !
     old_list_listed.append(string)
     return print_old_list(old_list_listed)
 
-def illegal_item_print(string):
+def illegal_item_print(string): #$ 7 !
     new_list = []
     for i in string:
         if len(i) < 3 or i.isalpha() == False:
@@ -44,16 +44,25 @@ def illegal_item_print(string):
             continue
     return new_list
 
-
-
-
-
+def duplicates_item_fromlist(string:list):
+    new_list = []
+    for i in string:
+        if i not in new_list:
+            new_list.append(i)
+        else:
+            continue
+    global old_list_listed
+    old_list_listed = new_list
+    return old_list_listed
 
 def quit(): #$ 9 ! 
     print("Goodbye!")
     return
 
-def Main_Menu():#% 2 - 
+
+
+
+while True:
     Menu = input("""Welcome to practice 7.2.6
 1. Print List
 2. List Length
@@ -68,48 +77,43 @@ Choose an action: """) #$ 1 !
     
     if Menu == "1": #$ 1 !
         print(print_old_list(old_list_listed))
-        Main_Menu()
 
     elif Menu == "2": #$ 2 !
         print(len_old_list(old_list_listed))
-        Main_Menu()
 
     elif Menu == "3": #% 3 !
         item = input("Which Item?: ") #! lower / higher case
         print(is_item_inlist(item))
-        Main_Menu()
 
     elif Menu =="4": #$ 4 !
         item = input(f"Which Item?: ")
         print(how_many_item(item))
-        Main_Menu()
 
     elif Menu == "5": #$ 5 !
         a = len_old_list(old_list_listed)
         item = int(input(f"""Which Item? 0 - {a - 1} : 
 {print_old_list(old_list_listed)} """))
         print(delete_item_fromlist(item))
-        Main_Menu()
 
     elif Menu == "6": #$ 6 !
         item = input("Which Item?: ")
         print(add_item_fromlist(item))
-        Main_Menu()
     
-    elif Menu == "7": #$ 6 !
+    elif Menu == "7": #$ 7 !
         print(print_old_list(illegal_item_print(old_list_listed)))
-        Main_Menu()
 
-    elif Menu == "8":
+    elif Menu == "8": #$ 8 !
+        duplicates_item_fromlist(old_list_listed)
+        print("Done!")
 
-
-    elif Menu == "9":
-        quit()
+    elif Menu == "9": #$ 9 !
+        print("Goodbye!")
+        break
 
     else:
         print("Invalid Input")
-        Main_Menu()
 
 
-Main_Menu()
+
+
 
