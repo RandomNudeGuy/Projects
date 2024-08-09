408ec29a144244a3f99b8b3a83923a325b1a72e3
Jonathan <julli626@gmail.com>
2024-08-09 20:53:50 +0300

    hangman

--- CampusIL/hangman_campusil.py
+++ CampusIL/hangman_campusil.py
@@ -65,8 +65,13 @@ STAGE_SEVEN = ("""
     |      / \\
     |""")
 
-old_letters = ['a', 'b']
 
+
+
+
+
+old_letters = []
+underlined_list = []
 def print_input(letter_guessed):   #% First  
     if word.isalpha() == False and word[1:] != "":
         print("E3")
@@ -110,25 +115,71 @@ def try_update_letter_guessed(letter_guessed, old_letters_guessed): #% Forth
         print("X\n" + new_list)
         return False
 
+# def word_guess_underline(string:list):
+#     word_length = len(string[0])
+#     underlined = "_ " * word_length
+#     print(string)
+#     return underlined
+    
+def show_hidden_word(secret_word:list, old_letters_guessed):
+    # word_length = len(secret_word[0])
+    # empty_space = "_ "
+    # underlined = []
+    # for i in range(word_length):
+    #     underlined.append(empty_space)
+    global underlined
+    global word
+    print(underlined)
+    for i in secret_word:
+        finder = int(secret_word[0].find(word))
+        print(finder)
+        if word in old_letters_guessed: #! already gueesed
+            print(underlined)
+            messege = "Letter already guessed"
+            return messege
+        elif word not in old_letters_guessed and word in secret_word[0]: #$ good guess
+            underlined[finder] = word
+            print(underlined)
+            messege = "Good guess!"
+            continue
+        elif word not in old_letters_guessed and word not in secret_word[0]: #% bad guess
+            print(underlined)
+            messege = "Try again"
+            return messege
+        else: #! how??
+            print(underlined)
+            messege = "Else"
+            return messege
 
-word = input("Please enter a word: ").lower()
-word = word.replace(" ", "")  #remeber to retrun
-print_input(word) #$ Start
-print(try_update_letter_guessed(word, old_letters))
+
+word_toguess = [input("Enter a word to guess: ")]
+
+word_length = len(word_toguess[0])
+empty_space = "_ "
+underlined = []
+for i in range(word_length):
+    underlined.append(empty_space)
+
+while True:
+    word = input("Please enter a letter: ").lower()
+    word = word.replace(" ", "")  #remeber to retrun
+    print_input(word) #$ Start
+    # print(word_guess_underline(word_toguess))
+    print(show_hidden_word(word_toguess, old_letters))
+    print(try_update_letter_guessed(word, old_letters))
+    
+    print(old_letters)
 
 
-print(old_letters)
 
 
+        
 #% Print letter > Print True if valid input type > Print True if valid input not in list > Print old letter List
 # print(word[1:])
 
 
 
 
-
-# * word_length = len(word)
-# * print("_ " * word_length)
 
 # first_try = input("Guess a letter: ").lower()
 # print(first_try)
