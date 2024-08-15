e7f2131d9164978a9ca9fa33413525b20c2ec57d
Jonathan <julli626@gmail.com>
2024-08-15 12:42:57 +0300

    omg finally 8.2.3 no oz no chatgpt only BIG BRAIN

--- CampusIL/campusil_8.2.3.py
+++ CampusIL/campusil_8.2.3.py
@@ -1,12 +1,14 @@
-first_tuple = (1, 2)
-second_tuple = (4, 5)
+first_tuple = (1, 2, 3)
+second_tuple = (4, 5, 6)
 long_tuple = ()
 def mult_tuple(tuple1, tuple2):
     for i in tuple1:
         for x in tuple2:
-            tuple = (i, x) 
+            tuple = ((i, x),)
+            rev_tuple = ((x, i),)
             global long_tuple
-            long_tuple += tuple
+            long_tuple += tuple 
+            long_tuple += rev_tuple
 
 
     return long_tuple
