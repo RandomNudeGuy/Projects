d8dad15294cdf6ea70ecdd7131013eca08205f05
Jonathan <julli626@gmail.com>
2024-08-19 20:26:23 +0300

    Projects

--- CampusIL/campusil3.py
+++
@@ -1,7 +0,0 @@
-word = input("Enter a word: ")
-length = len(word)
-length_halved = length // 2
-new_word_lower = word[:length_halved].lower()
-new_word_upper = word[length_halved:].upper()
-
-print(new_word_lower + new_word_upper)
--- CampusIL/campusil_7.2.5.py
+++
@@ -1,6 +0,0 @@
-def sequence_del(my_str):
-    for i in my_str:
-        if my_str.count(i) > 1:
-            xx
-
-
