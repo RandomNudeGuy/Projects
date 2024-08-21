8b2c6c1cca761432660b33a30dd6f512324dbf98
Jonathan <julli626@gmail.com>
2024-08-21 19:58:43 +0300

    fixed 8.3.4 to make it faster

--- CampusIL/campusil_8.3.4.py
+++ CampusIL/campusil_8.3.4.py
@@ -7,14 +7,10 @@ def inverse_dict(my_dict:dict):
     for key, value in my_dict.items():
         if value == 3:
             first_list.append(key)
+            reversed_dict[value] = first_list
         if value == 2:
             second_list.append(key)
-    for k, v in my_dict.items():
-        if v == 3:
-            reversed_dict[v] = first_list
-        if v == 2:
-            reversed_dict[v] = second_list
-    # print(first_list, second_list)
+            reversed_dict[value] = second_list
     return reversed_dict
 
 
