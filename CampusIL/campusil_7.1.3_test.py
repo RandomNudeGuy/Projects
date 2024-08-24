7e0571c8461c264c96a8e6073557dc6cef610358
Jonathan <julli626@gmail.com>
2024-08-24 15:52:11 +0300

    כ

--- CampusIL/campusil_7.1.3_test.py
+++
@@ -1,7 +0,0 @@
-i = 11
-
-while i > 0:
-    i -= 1    
-    if i == 5:
-        continue        
-    print(i)
--- CampusIL/test_dict.py
+++
@@ -1,3 +0,0 @@
-keyboard_info={'Are emoji included?': True, 'Language': 'Hebrew'}
-for info in keyboard_info.items():
-    print("%s -> %s" % info)
