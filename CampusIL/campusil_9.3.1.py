fc3ae77681a8f2c3aacf5da33d75f99cec4de4a1
Jonathan <julli626@gmail.com>
2024-09-08 19:04:26 +0300

    Yamma bardak

--- CampusIL/campusil_9.3.1.py
+++ CampusIL/campusil_9.3.1.py
@@ -6,6 +6,7 @@ def my_mp3_playlist(file_path):
     
     file_split = file_read.split("\n")
     cd_items = []
+    song_amount = 0
     
     for element in file_split:
         if element.strip():  # This checks if the line is not empty after stripping whitespace.
@@ -13,9 +14,36 @@ def my_mp3_playlist(file_path):
             cd_items.append(split_items)
 
     SongLength = []
+    SongName = []
     for i in cd_items:
+        song_amount += 1
+        SongName.append(i[1])
         SongLength.append(i[-1])
-    print(sorted(SongLength, reverse=True))
     
+    SongLengthSorted = sorted(SongLength, reverse=True)
+    for i in cd_items:
+        if i[-1] == SongLengthSorted[0]:
+            global SongLengthResult
+            SongLengthResult = i[0]
+    temp_sn_list = []
+    
+    for i in SongName:
+        first_counter = SongName.count(i)
+        if first_counter not in temp_sn_list:
+            temp_sn_list.append(first_counter)
+            temp_sn_list.sort()
+        else:
+            continue
+    for i in SongName:
+        if SongName.count(i) == temp_sn_list[-1]:
+            global SongNameResult
+            SongNameResult = i
+            break
+        else:
+            continue
+    
+    return (SongLengthResult, song_amount, SongNameResult)
 
+
+    
 print(my_mp3_playlist(r"/Users/mcrandom/Library/Mobile Documents/com~apple~CloudDocs/Python Projects/Projects/CampusIL/9.3.1 text file.txt"))
