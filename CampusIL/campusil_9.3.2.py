fed1779b8eb3bfb99c9afadba1ac99e0b13c39a2
Jonathan <julli626@gmail.com>
2024-09-08 21:05:35 +0300

    help me i cant

--- CampusIL/campusil_9.3.2.py
+++ CampusIL/campusil_9.3.2.py
@@ -1,17 +1,29 @@
 def my_mp4_playlist(file_path, new_song):
-    open_read = open(file_path, "r")
-    data = open_read.readlines() 
+    open_read = open(file_path, "r+")
     
-    ThirdLine = data[2]
+    while True:
+        data = open_read.readlines() 
+        data_len = len(data)
+        print(data_len)
+        if data_len >= 3:
+            ThirdLine = data[2]
+            cd_items = []
+            if ThirdLine.strip():  # This checks if the line is not empty after stripping whitespace.
+                split_items = [item for item in ThirdLine.split(";") if item]  # Skip empty strings
+                cd_items.append(new_song)
+                for i in split_items[1:]:
+                    cd_items.append(i)
+                # cd_items.append(split_items[1:])
+            cd_items = ";".join(cd_items)
+            break
+        else:
+            empty_lines = """1
+2
+3"""
+            open_read.writelines(empty_lines)
 
-    cd_items = []
-    if ThirdLine.strip():  # This checks if the line is not empty after stripping whitespace.
-        split_items = [item for item in ThirdLine.split(";") if item]  # Skip empty strings
-        cd_items.append(new_song)
-        for i in split_items[1:]:
-            cd_items.append(i)
-        # cd_items.append(split_items[1:])
-    cd_items = ";".join(cd_items)
+
+    
 
     data[2] = cd_items
     open_read.close()
