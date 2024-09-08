79a37a5d5ffaef09c13dcf3d94c6b3c956233698
Jonathan <julli626@gmail.com>
2024-09-08 20:22:31 +0300

    yamma

---
+++ CampusIL/campusil_9.3.2.py
@@ -0,0 +1,32 @@
+def my_mp4_playlist(file_path, new_song):
+    open_read = open(file_path, "r")
+    data = open_read.readlines() 
+    
+    ThirdLine = data[2]
+
+    cd_items = []
+    if ThirdLine.strip():  # This checks if the line is not empty after stripping whitespace.
+        split_items = [item for item in ThirdLine.split(";") if item]  # Skip empty strings
+        cd_items.append(new_song)
+        for i in split_items[1:]:
+            cd_items.append(i)
+        # cd_items.append(split_items[1:])
+    cd_items = ";".join(cd_items)
+
+    data[2] = cd_items
+    open_read.close()
+    open_write = open(file_path, "w")
+    open_write.writelines(data)
+    open_write.close()
+    open_read = open(file_path, "r")
+    data = open_read.read()
+    print(data)
+
+
+ 
+
+
+
+
+
+print(my_mp4_playlist(r"/Users/mcrandom/Library/Mobile Documents/com~apple~CloudDocs/Python Projects/Projects/CampusIL/9.3.1 text file.txt", "Python Love Story"))
