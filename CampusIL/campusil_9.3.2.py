f8ad6d2fed6ef2709c0024a0757d3df5f010cd70
Jonathan <julli626@gmail.com>
2024-09-09 10:19:09 +0300

    finaly

--- CampusIL/campusil_9.3.2.py
+++ CampusIL/campusil_9.3.2.py
@@ -2,24 +2,23 @@ def my_mp4_playlist(file_path, new_song):
     open_read = open(file_path, "r+")
     
     while True:
+        seeker = open_read.seek(0)
         data = open_read.readlines() 
         data_len = len(data)
         print(data_len)
         if data_len >= 3:
             ThirdLine = data[2]
             cd_items = []
-            if ThirdLine.strip():  # This checks if the line is not empty after stripping whitespace.
-                split_items = [item for item in ThirdLine.split(";") if item]  # Skip empty strings
-                cd_items.append(new_song)
-                for i in split_items[1:]:
-                    cd_items.append(i)
-                # cd_items.append(split_items[1:])
+            # if ThirdLine.strip():  # This checks if the line is not empty after stripping whitespace.
+            split_items = [item for item in ThirdLine.split(";") if item]  # Skip empty strings
+            cd_items.append(new_song)
+            for i in split_items[1:]:
+                cd_items.append(i)
+            # cd_items.append(split_items[1:])
             cd_items = ";".join(cd_items)
             break
         else:
-            empty_lines = """1
-2
-3"""
+            empty_lines = """\n\n\n"""
             open_read.writelines(empty_lines)
 
 
