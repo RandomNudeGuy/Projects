e5b515dd0a7db5ee875a3b9225c454e7c4d5ed82
Jonathan <julli626@gmail.com>
2024-09-10 15:23:20 +0300

    Code Practice

---
+++ PythonIDF/RandomModule-03.py
@@ -0,0 +1,22 @@
+import random
+import time
+
+Nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+code = random.choices(Nums, k=4)
+code = "".join(code)
+
+print(code)
+
+for i in range(0, 5):
+    codeinput = input("Enter Code:")
+    if i < 4:
+        if codeinput == code:
+            print("Good Job")
+            break
+        else:
+            print("Try Again")
+            continue
+    elif i == 4:
+        print("Too many failed attempts")
+        break
+
