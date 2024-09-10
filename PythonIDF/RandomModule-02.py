f38a5656ca54ffcf59b7f9d6acfc7ff9872153b9
Jonathan <julli626@gmail.com>
2024-09-10 14:06:26 +0300

    seventh question

--- PythonIDF/RandomModule-02.py
+++ PythonIDF/RandomModule-02.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 #$First Question
 #% firstNum = int(input("Enter first num:"))
@@ -33,17 +34,60 @@ import random
 
 #$Fourth Question
 
-def LuckyRoll(NumList):
-    Nums = random.choices(NumList, k = 3)
-    for i in Nums:
-        if i == 7:
-            return True
-        else:
-            for i in range(0, len(NumList)):
-                total = total + NumList[i]
-            if total == 7:
-                return True
-            else:
-                return False
+#% def LuckyRoll(NumList):
+#%     total = 0
+#%     Nums = random.choices(NumList, k = 3)
+#%     for i in Nums:
+#%         if i == 7:
+#%             print(Nums)
+#%             return True
+#%         else:
+#%             for i in range(0, len(NumList)):
+#%                 total = total + NumList[i]
+#%             if total == 7:
+#%                 print(Nums)
+#%                 return True
+#%             else:
+#%                 print(Nums)
+#%                 return False
 
-# NumList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#% NumList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#% print(LuckyRoll(NumList))
+
+#$Fifth Question
+
+#% def four_DiceRoll(Dice):
+#%     total = 0
+#%     for i in range(4):
+#%         result = random.choice(Dice)
+#%         time.sleep(1)
+#%         total = total + result
+#%         print(result)
+#%     return total
+
+
+#% DiceList = [1, 2, 3, 4, 5, 6]
+#% print(four_DiceRoll(DiceList))
+
+#$Sixth Question
+
+#% def two_numroll():
+#%     NumRoll1 = random.randrange(10, 101, 2)
+#%     NumRoll2 = random.randrange(10, 101, 2)
+
+#%     if NumRoll1 > NumRoll2:
+#%         msg = f"Num1 > {NumRoll1}, is bigger than Num2 > {NumRoll2}"
+#%         return msg
+#%     elif NumRoll2 == NumRoll1:
+#%         msg = f"Nums are even. {NumRoll1} == {NumRoll2}"
+#%         return msg
+#%     else:
+#%         msg = f"Num2 > {NumRoll2}, is bigger than Num1 > {NumRoll1}"
+#%         return msg
+    
+#% print(two_numroll())
+
+#$Seventh Question
+
+
+    
